e25d05fbb26f1e0a80584bf07fde56e09b510d21
MikeQin <mike.qin@gmail.com>
2019-06-15 03:08:08 -0400

    init

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Ninja React Context & Hooks Tutorial
 
-[Tutorial](https://youtu.be/6RhOzQciVwI?list=PL4cUxeGkcC9hNokByJilPg5g9m2APUePI)
+```
+npx create-react-app contextapp
+```
+* YouTube: [React Context & Hooks Tutorial](https://youtu.be/6RhOzQciVwI?list=PL4cUxeGkcC9hNokByJilPg5g9m2APUePI)
+* Github: [React Context & Hooks](https://github.com/iamshaunjp/react-context-hooks)
+
+### Complete React Course
+* YouTube: [Complete React Course](https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG)
+
---
+++ contextapp/src/contexts/ThemeContext.jsx
@@ -0,0 +1,30 @@
+import React, { Component, createContext } from "react";
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+  state = {
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" }
+  };
+
+  // Auto bind
+  toggleTheme = () => {
+    this.setState((prevState, props) => {
+      return { isLightTheme: !prevState.isLightTheme };
+    });
+  };
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+export default ThemeContextProvider;
