1c85f5b1b8551beabfdde78ec36de611227626a1
MikeQin <mike.qin@gmail.com>
2019-07-03 16:45:40 -0400

    init

--- booklist/src/contexts/BookContext.jsx
+++ booklist/src/contexts/BookContext.jsx
@@ -1,14 +1,21 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
   // const [books, setBooks] = useState([
   //   { title: 'Name of the Wind', author: 'Patrick Rothfuss', id: uuid() },
   //   { title: 'The Final Empire', author: 'Brandon Sanderson', id: uuid() }
   // ]);
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
